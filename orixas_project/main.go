e84896eb428ac9309f481e3ae4406006c7a8fe24
Abinercl <abiner17costa@gmail.com>
2024-04-25 16:44:55 -0300

    feat:making public functions

--- orixas_project/main.go
+++ orixas_project/main.go
@@ -9,20 +9,20 @@ func main() {
 	fmt.Print("Coloque sua Data de nascimento: ")
 	fmt.Scan(&dia, &mes, &ano) // lê cada uma das variaveis juntas
 	//chama a função somaDigitos para cada uma das variaveis
-	diaSoma := somaDigitos(dia)
-	mesSoma := somaDigitos(mes)
-	anoSoma := somaDigitos(ano)
+	diaSoma := SomaDigitos(dia)
+	mesSoma := SomaDigitos(mes)
+	anoSoma := SomaDigitos(ano)
 
 	total := diaSoma + mesSoma + anoSoma // A soma das somas dos dígitos de dia, mes e ano é calculada e armazenada na variável total.
 	if total > 16 {
-		total = somaDigitos(total)
+		total = SomaDigitos(total)
 
 	}
 	fmt.Println("valor total é: ", total)
 
 }
 
-func somaDigitos(numero int) int {
+func SomaDigitos(numero int) int {
 	soma := 0
 	for numero > 0 {
 		soma += numero % 10
