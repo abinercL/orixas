795d160ba734a3dbfbb41e251a1bc5c963d8d66e
Abinercl <abiner17costa@gmail.com>
2024-06-04 17:17:57 -0300

    Adicionando comentarios para explicação do codigo

--- orixas_project/main.go
+++ orixas_project/main.go
@@ -4,39 +4,40 @@ import (
 	"fmt"
 )
 
-var dia, mes, ano int
+var dia, mes, ano int // Declaração de variáveis globais
 
-// 1°parte
+// Função principal que recebe a data de nascimento, calcula a soma reduzida e chama a função SomaDigitos.
 func main() {
 
-	fmt.Print("Coloque sua Data de nascimento: ")
-	fmt.Scan(&dia, &mes, &ano) // lê cada uma das variaveis juntas
-	//chama a função somaDigitos para cada uma das variaveis
+	fmt.Print("Coloque sua Data de nascimento: ") // Solicita a data de nascimento ao usuário
+	fmt.Scan(&dia, &mes, &ano)                    // Lê os valores digitados pelo usuário
+
+	//calcula a soma das variaveis globais
 	diaSoma := SomaDigitos(dia)
 	mesSoma := SomaDigitos(mes)
 	anoSoma := SomaDigitos(ano)
 
-	total := diaSoma + mesSoma + anoSoma // A soma das somas dos dígitos de dia, mes e ano é calculada e armazenada na variável total.
-	if total > 16 {
+	total := diaSoma + mesSoma + anoSoma // Soma as somas individuais
+	if total > 16 {                      // Se a soma total for maior que 16, calcula a soma novamente
 		total = SomaDigitos(total)
-
 	}
-	fmt.Println("valor total é: ", total)
-
+	fmt.Println("valor total é: ", total) // Imprime o valor final da soma reduzida
 }
+
+// Função que calcula a soma dos dígitos de um número inteiro.
 func SomaDigitos(numero int) int {
-	soma := 0
-	for numero > 0 {
-		soma += numero % 10
-		numero /= 10
+	soma := 0        // Variável para armazenar a soma dos dígitos
+	for numero > 0 { // Loop para percorrer os dígitos do número
+		soma += numero % 10 // Adiciona o dígito atual à soma
+		numero /= 10        // Divide o número por 10 para obter o próximo dígito
 	}
-	return soma
+	return soma // Retorna a soma dos dígitos
 }
 
-//2°parte
+// 2°parte
 // Pegar os dados iniciais do input e numerar os caracteres em posiçoes de 1 a 8
-//func Impares_pares() {
+func Impares_pares(dataString string) {
 
-//}
+}
 
 //apos isso somar somente os numeros impar e depois os numeros pares de acordo com cada posição
